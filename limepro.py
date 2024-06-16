a97bf41eb0ebaf0453cb4c08774aa0362031afb8
AkashVA55 <84912090+AkashVA55@users.noreply.github.com>
2024-06-16 17:18:22 +0530

    Update limepro.py

--- limepro.py
+++ limepro.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Load and preprocess the image
-image = cv2.imread('C:/Users/AKASH V A/Downloads/simulator-windows-64/data/IMG/center_2024_05_09_00_13_13_911.jpg')  # Replace 'path_to_image.jpg' with the path to your image file
+image = cv2.imread('simulator-windows-64/data/IMG/center_2024_05_09_00_13_13_911.jpg')  # Replace 'path_to_image.jpg' with the path to your image file
 #image = cv2.GaussianBlur(image,  (3, 3), 0)
 image = cv2.resize(image, (200,66))
   # Resize the image to match the model's input shape
@@ -17,7 +17,7 @@ from lime import lime_image
 from keras.models import load_model
 # Initialize LIME explainer
 explainer = lime_image.LimeImageExplainer()
-model = load_model('C:/Users/AKASH V A/Downloads/model(1).h5')
+model = load_model('model(1).h5')
 # Generate explanations for the chosen instance
 explanation = explainer.explain_instance(image, model.predict, top_labels=1, hide_color=0, num_samples=1000)
 
